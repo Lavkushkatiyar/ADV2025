c3faf4b0f3cb1f1a17625be80e43e958eb4b7c3a
Lavkush Katiyar <lavkushkatiyar@Lavkushs-MacBook-Pro.local>
2025-12-15 14:56:53 +0530

    day 2 part 1 added

---
+++ day2/part1.js
@@ -0,0 +1,36 @@
+const leadingZero = (num) => (num[0] === "0");
+
+const existAfterIndex = (num, range, index) => range.includes(num, index);
+
+const isSequence = (num1, num2) => num1 === num2;
+
+const numberOfSequence = (num, sequence, index) => {
+  let count = 0;
+  let i = 0;
+  while (i < 10) {
+    if (num.indexOf(sequence, index) !== -1) {
+      index = num.indexOf(sequence, index);
+      console.log("i was here ", index);
+      count++;
+      i++;
+    } else break;
+  }
+
+  return count;
+};
+const findInvalidId = (num1) => {
+  for (let i = 0; i < num1.length; i++) {
+    const sequence = num1[i] + num1[i + 1];
+    if (existAfterIndex(sequence, num1, i + 1)) count++;
+  }
+};
+
+const invalidID = (range) => {
+  const sumOfInvalidId = 0;
+  const [start, end] = range.split("-");
+
+  if (leadingZero(start)) sumOfInvalidId += parseInt(start);
+  if (leadingZero(end)) sumOfInvalidId += parseInt(end);
+  for (let i = start; i < end; i++) {
+  }
+};
